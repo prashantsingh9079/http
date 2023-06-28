01aca1b46718dfca4328a9d33ada0d2c710e9583
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-28 23:31:38 +0530

    error handling

--- fetchInReact/src/App.js
+++ fetchInReact/src/App.js
@@ -1,32 +1,60 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
 
-  const [movies,setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
-  async function dummyMovies()
-  {
-    setIsLoading(true)
-    const res = await fetch("https://swapi.dev/api/films");
-    
-    const res2 = await res.json();
+  const [customError, setCustomError] = useState(null);
+  const [retry,setRetry] = useState(true)
 
-    setMovies(res2.results)
+  async function dummyMovies() {
+
+    setIsLoading(true)
+    setCustomError(null)
+    try {
+      const res = await fetch("https://swapi.dev/api/film");
+     
+      console.log(res)
+      if (res.ok === false) {
+        throw new customError("something went wrong retrying...")
+      }
+      const res2 = await res.json();
+      
+      console.log(res2);
+      
+
+      setMovies(res2.results)
+      setIsLoading(false)
+    }
+    catch(error)
+    {
+      setCustomError(error.message)
+    }
+    if(!retry)
     setIsLoading(false)
+
+    
+  }
+
+  function stopRetry()
+  {
+    setIsLoading(false)
+    setRetry(true)
   }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={dummyMovies}>Fetch Movies</button>
+        <button onClick={stopRetry}>Stop retrying</button>
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
         {isLoading && <p>Loading data....</p>}
+        {!isLoading && customError && <p>{customError}</p> }
       </section>
     </React.Fragment>
   );
